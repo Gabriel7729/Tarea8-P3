83df2de78c14a3818472ebd75c2241907d11ae25
Anthony-Valdez <anthony_valdez012@hotmail.com>
2021-03-21 09:55:10 -0400

    Correccion de notificaciones

--- obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -12,112 +12,112 @@ namespace Tarea8_P3.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "c:\Users\Lusan\Desktop\Tarea8-P3\_Imports.razor"
+#line 1 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "c:\Users\Lusan\Desktop\Tarea8-P3\_Imports.razor"
+#line 2 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "c:\Users\Lusan\Desktop\Tarea8-P3\_Imports.razor"
+#line 3 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "c:\Users\Lusan\Desktop\Tarea8-P3\_Imports.razor"
+#line 4 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "c:\Users\Lusan\Desktop\Tarea8-P3\_Imports.razor"
+#line 5 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "c:\Users\Lusan\Desktop\Tarea8-P3\_Imports.razor"
+#line 6 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "c:\Users\Lusan\Desktop\Tarea8-P3\_Imports.razor"
+#line 7 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "c:\Users\Lusan\Desktop\Tarea8-P3\_Imports.razor"
+#line 8 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "c:\Users\Lusan\Desktop\Tarea8-P3\_Imports.razor"
+#line 9 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\_Imports.razor"
 using Tarea8_P3;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "c:\Users\Lusan\Desktop\Tarea8-P3\_Imports.razor"
+#line 10 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\_Imports.razor"
 using Tarea8_P3.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "c:\Users\Lusan\Desktop\Tarea8-P3\_Imports.razor"
+#line 11 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\_Imports.razor"
 using Radzen;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "c:\Users\Lusan\Desktop\Tarea8-P3\_Imports.razor"
+#line 12 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\_Imports.razor"
 using Radzen.Blazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "c:\Users\Lusan\Desktop\Tarea8-P3\_Imports.razor"
+#line 13 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\_Imports.razor"
 using System.Net;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "c:\Users\Lusan\Desktop\Tarea8-P3\_Imports.razor"
+#line 14 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\_Imports.razor"
 using System.Collections.Generic;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 15 "c:\Users\Lusan\Desktop\Tarea8-P3\_Imports.razor"
+#line 15 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\_Imports.razor"
 using Newtonsoft.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "c:\Users\Lusan\Desktop\Tarea8-P3\Pages\Index.razor"
+#line 2 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\Pages\Index.razor"
 using Data;
 
 #line default
@@ -132,7 +132,7 @@ using Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 132 "c:\Users\Lusan\Desktop\Tarea8-P3\Pages\Index.razor"
+#line 132 "C:\Users\a\Downloads\Telegram Desktop\Tarea8-P3\Tarea8-P3\Pages\Index.razor"
         List<vacunados> Products = new List<vacunados>();
 
     protected override async Task OnInitializedAsync()
